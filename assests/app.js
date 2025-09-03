7c9d7d26c08a73a55a9b7731b6f118a7304002cf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-02 22:37:35 -0300

    style: players card

--- assests/app.js
+++ assests/app.js
@@ -1,49 +1,49 @@
-const jogadoras = [
+const PLAYERS = [
   {
-    nome: "Andressa Alves",
-    posicao: "Meio-campo",
-    clube: "Corinthians",
-    foto: "https://example.com/andressa.jpg",
+    nome: "Marta",
+    posicao: "ATA",
+    clube: "https://upload.wikimedia.org/wikipedia/pt/c/c0/OrlandoCity_SC_logo.png",
+    foto: "https://static.wefut.com/assets/images/fut24/playeravatars/custom/50558538.png",
     gols: 15,
     assistencias: 10,
     jogos: 28,
     favorita: false,
   },
   {
-    nome: "Dayana Rodríguez",
-    posicao: "Meio-campo",
-    clube: "Corinthians",
-    foto: "https://example.com/dayana.jpg",
+    nome: "Aitana Bonmatí",
+    posicao: "MEI",
+    clube: "https://upload.wikimedia.org/wikipedia/pt/thumb/4/43/FCBarcelona.svg/2020px-FCBarcelona.svg.png",
+    foto: "https://cdn.futwiz.com/assets/img/fc24/faces/117682179.png",
     gols: 5,
     assistencias: 12,
     jogos: 30,
     favorita: false,
   },
   {
-    nome: "Mariza",
-    posicao: "Zagueira",
-    clube: "Corinthians",
-    foto: "https://example.com/mariza.jpg",
+    nome: "Alex Morgan",
+    posicao: "ATA",
+    clube: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgDIEuby3BvZue9cwVBi7WtEI6dbuHAPwQpSvXI7WkwfgzT56zudBeLGDdUjz9UCN7-es5C4Za2DZZzj7BH1_huKeFYd-f1AUncyy4CR2HZorzqZr_-wiktWik3whKX97PKIwBzvycdKLk/s1600/1646909251708930-0.png",
+    foto: "https://cdn3.futbin.com/content/fifa24/img/players/p134444029.png?fm=png&ixlib=java-2.1.0&w=224&s=5a3a3079c8cfb7a03037583931df64cb",
     gols: 2,
     assistencias: 1,
     jogos: 32,
     favorita: false,
   },
   {
-    nome: "Thaís Regina",
-    posicao: "Zagueira",
-    clube: "Corinthians",
-    foto: "https://example.com/thais.jpg",
+    nome: "Renard",
+    posicao: "ZAG",
+    clube: "https://fifastatic.fifaindex.com/FIFA22/teams/light/66.png",
+    foto: "https://cdn3.futbin.com/content/fifa25/img/players/p67336180.png?fm=png&ixlib=java-2.1.0&verzion=1&w=485&s=161eea8ea4a4c7c9d11fef7d6a0fe607",
     gols: 1,
     assistencias: 2,
     jogos: 25,
     favorita: false,
   },
   {
-    nome: "Letícia Teles",
-    posicao: "Zagueira",
-    clube: "Corinthians",
-    foto: "https://example.com/leticia.jpg",
+    nome: "Alex Greenwood",
+    posicao: "LE",
+    clube: "https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/England_national_football_team_crest.svg/250px-England_national_football_team_crest.svg.png",
+    foto: "https://cdn3.futbin.com/content/fifa24/img/players/p50558902.png?fm=png&ixlib=java-2.1.0&w=224&s=ed308190da7122c0952a310ed97fa3bc",
     gols: 0,
     assistencias: 0,
     jogos: 18,
@@ -51,84 +51,154 @@ const jogadoras = [
   },
 ];
 
+// Create
 const addPlayer = () => {
-  const namePlayer = document.querySelector("#namePlayer").value;
+  const playerForm = document.querySelector("#playerForm");
+
+  const playerName = document.querySelector("#namePlayer").value;
   const playerTeam = document.querySelector("#playerTeam").value;
   const playerPicture = document.querySelector("#playerPicture").value;
   const playerAssists = document.querySelector("#playerAssists").value;
   const playerGoals = document.querySelector("#playerGoals").value;
   const playerPostion = document.querySelector("#playerPostion").value;
-  const favorite = document.querySelector("#favorite").value;
   const playerGames = document.querySelector("#playerGames").value;
 
+  if (!playerForm.reportValidity()) {
+    console.warn("Todos os campos obrigatórios devem ser preenchidos");
+    return;
+  }
+
   const player = {
-    nome: namePlayer,
+    nome: playerName,
     posicao: playerPostion,
     clube: playerTeam,
     foto: playerPicture,
     gols: playerGoals,
     assistencias: playerAssists,
     jogos: playerGames,
-    favorita: favorite,
+    favorita: false,
   };
 
-  jogadoras.unshift(player);
-  document.querySelector("#playerForm").reset();
+  PLAYERS.unshift(player);
+  playerForm.reset();
   listPlayers();
+
+  alert("Jogadora criada com sucesso!");
 };
 
-//Read
+// Read
 const listPlayers = () => {
   const playersList = document.querySelector(".playersList");
   playersList.innerHTML = " ";
 
-  jogadoras.forEach((jogadora, index) => {
+  PLAYERS.forEach((jogadora, index) => {
     const cardPlayers = document.createElement("div");
     cardPlayers.classList.add("cardPlayer");
 
     cardPlayers.innerHTML = `
-    
-    <div>
-      <img src ="${jogadora.foto}"> 
-      <button class= "buttonEdit" data-section = "image" data-action="edit" data-index="${index}">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button> 
+    <div class="playerInfo"> 
+        <img src="${jogadora.foto}" class="playerImage"> 
+        <p class="playerPosition"> ${jogadora.posicao} </p>
+        <img src="${jogadora.clube}" class="playerTeam">
+        <p class="playerName">${jogadora.nome.toUpperCase()} </p>
     </div>
-    <div class = "cardSections">
-      <p>${jogadora.nome} </p> 
-      <button class= "buttonEdit" data-section = "nome" data-action="edit" data-index="${index}">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button> 
+    <div class="playerStatistics">
+        <p> <strong> Jogos: </strong> ${jogadora.jogos}  
+         
+        </p>
+        <p> <strong> Gols: </strong> ${jogadora.gols} 
+          <button class="buttonEdit" data-section="gols" data-action="edit" data-index="${index}">
+            <i class="fa-solid fa-pen-to-square"></i>
+          </button> 
+        </p>
+        <p> <strong> Assis.: </strong> ${jogadora.assistencias}  
+          <button class="buttonEdit" data-section="assistencias" data-action="edit" data-index="${index}">
+            <i class="fa-solid fa-pen-to-square"></i>
+          </button>  
+        </p>
     </div>
-    <div class = "cardSections">
-      <p>${jogadora.posicao} </p>
-      <button class= "buttonEdit" data-section = "posicao" data-action="edit" data-index="${index}">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button> 
-    </div>
-    <div class = "cardSections">
-      <p>${jogadora.assistencias} </p>
-      <button class= "buttonEdit" data-section = "assistencias" data-action="edit" data-index="${index}">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button> 
-    </div>
-    <div class = "cardSections">
-       <p>${jogadora.clube} </p>
-      <button class= "buttonEdit" data-section = "clube" data-action="edit" data-index="${index}">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button> 
-    </div>
-    <div class = "cardSections">
-      <p>${jogadora.gols} </p>
-      <button class= "buttonEdit" data-section = "gols" data-action="edit" data-index="${index}">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button> 
-    </div>
-  
-    <button data-action="delete" data-index="${index}"> Apagar </button>`;
+    `;
 
     playersList.append(cardPlayers);
   });
+};
+
+// Update
+const editPlayer = (index, section) => {
+  let novoCampo = "";
+
+  switch (section) {
+    case "clube":
+      novoCampo = prompt("Editar Clube:", PLAYERS[index].clube);
+      if (novoCampo !== null) {
+        PLAYERS[index].clube = novoCampo;
+        listPlayers();
+      }
+      break;
+
+    case "nome":
+      novoCampo = prompt("Editar Nome:", PLAYERS[index].nome);
+      if (novoCampo !== null) {
+        PLAYERS[index].nome = novoCampo;
+        listPlayers();
+      }
+
+      break;
+
+    case "posicao":
+      novoCampo = prompt("Editar Posição:", PLAYERS[index].posicao);
+      if (novoCampo !== null) {
+        PLAYERS[index].posicao = novoCampo;
+        listPlayers();
+      }
+
+      break;
+
+    case "assistencias":
+      novoCampo = prompt(
+        "Editar N° de Asistencias:",
+        PLAYERS[index].assistencias
+      );
+      if (novoCampo !== null) {
+        PLAYERS[index].assistencias = novoCampo;
+        listPlayers();
+      }
+
+      break;
+
+    case "gols":
+      novoCampo = prompt("Editar N° de gols:", PLAYERS[index].gols);
+      if (novoCampo !== null) {
+        PLAYERS[index].gols = novoCampo;
+        listPlayers();
+      }
+
+      break;
+
+    case "image":
+      novoCampo = prompt("Editar imagem(url):", PLAYERS[index].foto);
+      if (novoCampo !== null) {
+        PLAYERS[index].foto = novoCampo;
+        listPlayers();
+      }
+      break;
+
+    default:
+      alert("Erro");
+      break;
+
+      alert("Campo atualizado com sucesso!");
+  }
+};
+
+// Delete
+const deletePlayer = (index) => {
+  const confirm = window.confirm("Tem certeza que deseja apagar este post?");
+
+  if (confirm) {
+    PLAYERS.splice(index, 1);
+    listPlayers();
+  }
 };
 
 const handleCardClick = (event) => {
@@ -147,82 +217,9 @@ const handleCardClick = (event) => {
   }
 };
 
-const editPlayer = (index, section) => {
-  let novoCampo = "";
-
-  switch (section) {
-    case "clube":
-      novoCampo = prompt("Editar Clube:", jogadoras[index].clube);
-      if (novoCampo !== null) {
-        jogadoras[index].clube = novoCampo;
-        listPlayers();
-      }
-      break;
-
-    case "nome":
-      novoCampo = prompt("Editar Nome:", jogadoras[index].nome);
-      if (novoCampo !== null) {
-        jogadoras[index].nome = novoCampo;
-        listPlayers();
-      }
-
-      break;
-
-    case "posicao":
-      novoCampo = prompt("Editar Posição:", jogadoras[index].posicao);
-      if (novoCampo !== null) {
-        jogadoras[index].posicao = novoCampo;
-        listPlayers();
-      }
-
-      break;
-
-    case "assistencias":
-      novoCampo = prompt(
-        "Editar N° de Asistencias:",
-        jogadoras[index].assistencias
-      );
-      if (novoCampo !== null) {
-        jogadoras[index].assistencias = novoCampo;
-        listPlayers();
-      }
-
-      break;
-
-    case "gols":
-      novoCampo = prompt("Editar N° de gols:", jogadoras[index].gols);
-      if (novoCampo !== null) {
-        jogadoras[index].gols = novoCampo;
-        listPlayers();
-      }
-
-      break;
-
-    case "image":
-      novoCampo = prompt("Editar imagem(url):", jogadoras[index].foto);
-      if (novoCampo !== null) {
-        jogadoras[index].foto = novoCampo;
-        listPlayers();
-      }
-      break;
-
-    default:
-      alert("Erro");
-      break;
-  }
-};
-
-const deletePlayer = (index) => {
-  const confirm = window.confirm("Tem certeza que deseja apagar este post?");
-
-  if (confirm) {
-    jogadoras.splice(index, 1);
-    listPlayers();
-  }
-};
-
 window.onload = () => {
   listPlayers();
+
   document
     .querySelector(".playersList")
     .addEventListener("click", handleCardClick);
